3ca91656584618c170033895a8392d840a6fbefc
keke0123 <keke0123@naver.com>
2019-01-17 13:33:35 +0900

    id check 기능 넣음 / id check validation check 하는중

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -4,19 +4,20 @@ import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms'
 import {HTTP} from '@ionic-native/http';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-
 @IonicPage()
 @Component({
   selector: 'page-signup',
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  // input 으로 받은 내용 양방향 저장
   data:any = {
     id:'',
     password:'',
     email:''
   }
 
+  // 폼 체크후 에러시 에러 데이타에 있는 문자열이 출력된다.
   errorData:any = {
     userId:'id 를 입력해주세요',
     userPassword:'Password 를 입력해주세요',
@@ -37,11 +38,33 @@ export class SignupPage {
     });
  
   }
-
+  // 아이디 중복 확인할 함수 / input id blur 일때 이벤트
+  checkId(){
+    console.log("checkId");
+    this.httpd.get('http://192.168.0.3:8888/project/checkid.do',
+    {
+      // JSON.stringfy 쓰고 싶으면 header 에 content-type에 charset까지 명시
+      params:{
+        id:this.data.id
+      }
+    }).toPromise()
+    .then(data => {
+      console.log(data);
+      //this.signupForm.controls['userId'].setErrors({'cantuse':false});
+      if(data.result=='fail'){
+        console.log("사용불가");
+        //this.signupForm.controls['userId'].setErrors({'cantuse':true});
+      }
+    })
+    .catch();
+  }
+  // submit 버튼을 눌렀을때 동작
   submitSignup(){
     this.triedToSubmit = true;
+    // 만약 폼 에러시
     if(this.signupForm.invalid){
       for(var control in this.signupForm.controls){
+        // dirty, touch 되었다고 만든다.
         this.signupForm.controls[control].markAsDirty();
         this.signupForm.controls[control].markAsTouched();
       }
@@ -63,8 +86,8 @@ export class SignupPage {
     // }).catch(error => {
     //   //this.testString=error.error;
     // });
-
-    const httpOptions = {
+    // header 값을 담을 객체
+    let httpOptions = {
       headers: new HttpHeaders({
         // charset까지 적어주면 json.stringify 해도 들어간다.
         //'Content-Type':  'application/json; charset=utf-8',
@@ -85,19 +108,21 @@ export class SignupPage {
       id:this.data.id,
       password:this.data.password,
       email:this.data.email
-
     },httpOptions).toPromise()
     .then(data => {
       console.log(data);
     })
     .catch();
     
+    // 회원가입이 끝나면 원래 페이지로 이동시킨다.
+    this.navCtrl.pop();
   }
-
+  // 폼 에러 체크후 상황에 맞는 문자를 저장해둔 객체
   errorMessages = {
     userId: {
       required: 'id 를 입력해주세요',
-      minlength: '최소 4글자 이상 입력해주세요'
+      minlength: '최소 4글자 이상 입력해주세요',
+      cantuse: '이미 사용중인 아이디 입니다'
     },
     userEmail:{
       required:'Email 을 입력해주세요',
@@ -120,10 +145,6 @@ export class SignupPage {
         this.errorData[field]=this.errorMessages[field][validator];
       }
     }
-  }
-
-  signSub(){
-    
   }
 
   ionViewDidLoad() {
